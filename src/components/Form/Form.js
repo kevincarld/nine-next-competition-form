0ddfaa6bea0c76310062d5fa79983b3b495f21ca
Kevin David <kevincarld@gmail.com>
2023-02-23 11:25:21 +1100

    Added state for submission

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -26,12 +26,15 @@ import { Select } from 'chakra-react-select';
 import Container from '../util/Container';
 import { isFieldError, isFieldTouched, validateCheckboxTrue } from 'utils/common';
 import { initialFormValues, validationSchema } from './util/schema';
+import Thankyou from './Thankyou';
 
 export default function FormComponent() {
+  const [formSubmitted, setFormSubmitted] = React.useState(false)
 
   const handleFormSubmit = async (formik) => {
-    const isAccepted = validateCheckboxTrue(formik, 'ACCEPTANCE', 'Please click this checkbox to accept.')
+    formik.setSubmitting(true)
 
+    const isAccepted = validateCheckboxTrue(formik, 'ACCEPTANCE', 'Please click this checkbox to accept.')
     if(isAccepted && formik.isValid && formik.dirty) {
 
       const { values } = formik
@@ -46,125 +49,132 @@ export default function FormComponent() {
       // axios.post('https://submit.formstack.com/forms/YOUR_FORM_ID', values)
       // .then(response => {
       //   console.log(response);
-      //   actions.setSubmitting(false);
+      //     formik.setSubmitting(false);
+      //      setFormSubmitted(true)
       // })
       // .catch(error => {
-      //   console.log(error);
-      //   actions.setSubmitting(false);
+      //  console.log(error);
+      //  formik.setSubmitting(false)
+      //  setFormSubmitted(true)
       // });
+
+      formik.setSubmitting(false)
+      setFormSubmitted(true)
     }
 
+    formik.setSubmitting(false)
   }
 
   return (
     <Container>
+      {formSubmitted ? <Thankyou />
+      :
+        <Formik
+          initialValues={initialFormValues}
+          validationSchema={validationSchema}
+          onSubmit={(values, actions) => {
+            return true // give responsibility to handleFormSubmit()
+          }}
+          >
 
-      <Formik
-        initialValues={initialFormValues}
-        validationSchema={validationSchema}
-        onSubmit={(values, actions) => {
-          return true // give responsibility to handleFormSubmit()
-        }}
-        >
+          {(formik) => {
+            const invalidFname = isFieldTouched(formik, 'FIRST_NAME') && isFieldError(formik, 'FIRST_NAME')
+            const invalidLname = isFieldTouched(formik, 'LAST_NAME') && isFieldError(formik, 'LAST_NAME')
 
-        {(formik) => {
-          const invalidFname = isFieldTouched(formik, 'FIRST_NAME') && isFieldError(formik, 'FIRST_NAME')
-          const invalidLname = isFieldTouched(formik, 'LAST_NAME') && isFieldError(formik, 'LAST_NAME')
+            const selectOptions = [
+              {label: 'Select A', value: 'select a'},
+              {label: 'Select B', value: 'select b'},
+            ]
 
-          const selectOptions = [
-            {label: 'Select A', value: 'select a'},
-            {label: 'Select B', value: 'select b'},
-          ]
+            return(
+              <Form>
+                <Box
+                  maxW='750px'
+                  mx='auto'
+                  mt='40px'
+                  p={8}
+                >
+                  <Stack spacing={4}>
+                    <Flex gap={4}>
+                      <FormControl id="FIRST_NAME" isRequired isInvalid={invalidFname}>
+                        <FormLabel htmlFor={`FIRST_NAME`}>Enter your first name</FormLabel>
+                        <Field
+                          as={Input}
+                          type="text"
+                          id="FIRST_NAME"
+                          name="FIRST_NAME"
+                          placeholder='First name *'
+                        />
+                        {invalidFname && <FormErrorMessage>{formik?.errors?.FIRST_NAME}</FormErrorMessage>}
+                      </FormControl>
 
-          return(
-            <Form>
-              <Box
-                maxW='750px'
-                mx='auto'
-                mt='40px'
-                p={8}
-              >
-                <Stack spacing={4}>
-                  <Flex gap={4}>
-                    <FormControl id="FIRST_NAME" isRequired isInvalid={invalidFname}>
-                      <FormLabel htmlFor={`FIRST_NAME`}>Enter your first name</FormLabel>
-                      <Field
-                        as={Input}
-                        type="text"
-                        id="FIRST_NAME"
-                        name="FIRST_NAME"
-                        placeholder='First name *'
-                      />
-                      {invalidFname && <FormErrorMessage>{formik?.errors?.FIRST_NAME}</FormErrorMessage>}
-                    </FormControl>
+                      <FormControl id="LAST_NAME" isRequired isInvalid={invalidLname}>
+                        <FormLabel htmlFor={`LAST_NAME`}>Enter your last name</FormLabel>
+                        <Field
+                          as={Input}
+                          type="text"
+                          id="LAST_NAME"
+                          name="LAST_NAME"
+                          placeholder='Last name *'
+                        />
+                        {invalidLname && <FormErrorMessage>{formik?.errors?.LAST_NAME}</FormErrorMessage>}
+                      </FormControl>
+                    </Flex>
 
-                    <FormControl id="LAST_NAME" isRequired isInvalid={invalidLname}>
-                      <FormLabel htmlFor={`LAST_NAME`}>Enter your last name</FormLabel>
-                      <Field
-                        as={Input}
-                        type="text"
-                        id="LAST_NAME"
-                        name="LAST_NAME"
-                        placeholder='Last name *'
-                      />
-                      {invalidLname && <FormErrorMessage>{formik?.errors?.LAST_NAME}</FormErrorMessage>}
-                    </FormControl>
-                  </Flex>
+                    <Field name="SAMPLE_SELECT">
+                      {({ field, form, meta }) => {
+                        return (
+                          <FormControl id={field.name} isRequired isInvalid={meta?.error !== '' || meta?.error !== undefined}>
+                            <FormLabel htmlFor={`SAMPLE_SELECT`}>Sample dropdown select</FormLabel>
+                            <Select
+                              instanceId={field.name}
+                              id={field.name}
+                              name={field.name}
+                              placeholder="Select your answer"
+                              options={selectOptions}
+                              value={
+                                selectOptions
+                                  ? selectOptions.find((option) => option.value === field.value)
+                                  : null
+                              }
+                              onChange={ (option) => form.setFieldValue(field.name, option.value) }
+                              onBlur={ () => {
+                                form.validateField(field.name)
+                                form.setFieldTouched(field.name, true)
+                              }}
+                              />
 
-                  <Field name="SAMPLE_SELECT">
-                    {({ field, form, meta }) => {
-                      return (
-                        <FormControl id={field.name} isRequired isInvalid={meta?.error !== '' || meta?.error !== undefined}>
-                          <FormLabel htmlFor={`SAMPLE_SELECT`}>Role</FormLabel>
-                          <Select
-                            instanceId={field.name}
-                            id={field.name}
-                            name={field.name}
-                            placeholder="Select your role"
-                            options={selectOptions}
-                            value={
-                              selectOptions
-                                ? selectOptions.find((option) => option.value === field.value)
-                                : null
-                            }
-                            onChange={ (option) => form.setFieldValue(field.name, option.value) }
-                            onBlur={ () => {
-                              form.validateField(field.name)
-                              form.setFieldTouched(field.name, true)
-                            }}
-                            />
+                              {meta?.error && meta?.touched && <FormErrorMessage>{meta?.error}</FormErrorMessage> }
+                          </FormControl>
+                        )}
+                      }
+                    </Field>
 
-                            {meta?.error && meta?.touched && <FormErrorMessage>{meta?.error}</FormErrorMessage> }
-                        </FormControl>
-                      )}
-                    }
-                  </Field>
-
-                  <Field as={Checkbox} type='checkbox' name='ACCEPTANCE' borderColor={'black'} isInvalid={formik?.values?.ACCEPTANCE === false && formik?.errors?.ACCEPTANCE}>
-                    Acceptance sample.
-                  </Field>
-                  {formik?.values?.ACCEPTANCE === false && formik?.errors?.ACCEPTANCE && <Text mt='2!important' fontSize='small' color='red'>{formik?.errors?.ACCEPTANCE}</Text>}
+                    <Field as={Checkbox} type='checkbox' name='ACCEPTANCE' borderColor={'black'} isInvalid={formik?.values?.ACCEPTANCE === false && formik?.errors?.ACCEPTANCE}>
+                      Acceptance sample.
+                    </Field>
+                    {formik?.values?.ACCEPTANCE === false && formik?.errors?.ACCEPTANCE && <Text mt='2!important' fontSize='small' color='red'>{formik?.errors?.ACCEPTANCE}</Text>}
 
 
-                  <Box>
-                    <Button
-                      isDisabled={formik.isSubmitting || !formik.dirty || (formik.dirty && !formik.isValid)}
-                      onClick={() => handleFormSubmit(formik)}
-                      isLoading={formik.isSubmitting}
-                      spinner={<Spinner />}
-                    >
-                      Submit
-                    </Button>
-                  </Box>
+                    <Box>
+                      <Button
+                        isDisabled={formik.isSubmitting || !formik.dirty || (formik.dirty && !formik.isValid)}
+                        onClick={() => handleFormSubmit(formik)}
+                        isLoading={formik.isSubmitting}
+                        spinner={<Spinner />}
+                      >
+                        Submit
+                      </Button>
+                    </Box>
 
-                </Stack>
-              </Box>
-            </Form>
-          )
-        }}
+                  </Stack>
+                </Box>
+              </Form>
+            )
+          }}
 
-      </Formik>
-
+        </Formik>
+      }
     </Container>
   )
 }
---
+++ src/components/Form/Thankyou.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Box, Text } from '@chakra-ui/react'
+import Container from 'components/util/Container'
+
+export default function Thankyou() {
+
+  return (
+    <Container py='50px' textAlign='center'>
+      <Text as='h2' fontSize='40px'>This is a sample thank you message for submitting the form.</Text>
+    </Container>
+  )
+}
+
+// font styles
+const texts = {
+  xx: {
+
+  }
+}
+
