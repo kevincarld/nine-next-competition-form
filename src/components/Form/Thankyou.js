16ab9006e287b31bf11f9b5e376598cf579fab44
Kevin David <kevincarld@gmail.com>
2023-02-23 12:39:11 +1100

    Update thank you message to look better

--- src/components/Form/Thankyou.js
+++ src/components/Form/Thankyou.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Text, Alert, AlertIcon, AlertTitle, AlertDescription } from '@chakra-ui/react'
 import Container from 'components/util/Container'
 
 export default function Thankyou() {
 
   return (
     <Container py='50px' textAlign='center'>
-      <Text as='h2' fontSize='40px'>This is a sample thank you message for submitting the form.</Text>
+      <Alert
+        status='success'
+        variant='subtle'
+        flexDirection='column'
+        alignItems='center'
+        justifyContent='center'
+        textAlign='center'
+        p='60px 20px 60px'
+        maxW='750px'
+        mx='auto'
+      >
+        <AlertIcon boxSize='40px' mr={0} />
+        <AlertTitle mt={4} mb={2} fontSize='lg'>
+          Application submitted!
+        </AlertTitle>
+        <AlertDescription maxWidth='sm'>
+          This is a sample description message. Thanks for submitting your application. Our team will get back to you soon.
+        </AlertDescription>
+      </Alert>
     </Container>
   )
 }
